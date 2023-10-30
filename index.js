3dcf8024f84f3aadfac8269b8424edbb49dddd6a
AnkitKumar <ankit17110747@yahoo.com>
2023-10-30 08:35:47 +0530

    basic web chat

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const server = http.createServer(app);
 const {Server} = require('socket.io')
 const io = new Server(server);
 
-
-
 app.use('/', express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    socket.on('user-message', (message)=>{
+        socket.broadcast.emit('message', message);
+    })
 });
 server.listen(9000, ()=>{
     console.log('server started');
